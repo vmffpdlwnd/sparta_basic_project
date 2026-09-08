e14723d9b8fdda63c3d985fa7b19ae6e137032dc
vmffpdlwnd <leegunehee@gmail.com>
2026-09-08 11:18:16 +0900

    Lv 5. Bean Validation: 게임 생성
    저장된 게임을 찾을 수 없습니다. 저장 목록으로 돌아갑니다.

--- src/main/java/com/gamebasic/runcard/dto/CardResponse.java
+++ src/main/java/com/gamebasic/runcard/dto/CardResponse.java
@@ -4,8 +4,14 @@ import lombok.Getter;
 
 @Getter
 public class CardResponse {
-    // TODO (Lv 5): API 명세의 카드 응답 JSON에 맞게 필드를 만들고 생성자에서 채우세요.
+
+    private final Long id;
+    private final String cardType;
+    private final int acquiredFloor;
 
     public CardResponse(Long id, String cardType, int acquiredFloor) {
+        this.id = id;
+        this.cardType = cardType;
+        this.acquiredFloor = acquiredFloor;
     }
 }
--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,18 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RunCardRequest {
-    // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    @NotBlank
     private String cardType;
+
+    @NotNull
     private Integer acquiredFloor;
 }
